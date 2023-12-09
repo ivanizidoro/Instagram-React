7b8d1723ace478c6dc00060f97535787a1d4478d
ivanizidoro <ivanizidoro@hotmail.com>
2023-12-09 19:14:02 -0300

    adicionado Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from "react"; 
+import { useState } from "react";
 
 import styled, { ThemeProvider } from "styled-components";
+
 import { NavBar } from "./components/navbar";
-import { Screen} from "./style";
+import { Header } from "./components/header"
+
+
+import { Flex, Screen } from "./style";
 import { darkTheme, lightTheme } from "./style/theme";
 
 function App() {
-  const [ theme, setTheme ] = useState('dark');
+  const [theme, setTheme] = useState('dark');
 
   const themeToggler = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light');
@@ -17,7 +21,12 @@ function App() {
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
 
       <Screen>
-          <NavBar themeToggler={themeToggler} theme={theme}/>
+        <NavBar themeToggler={themeToggler} theme={theme} />
+
+        <Flex gap="2px">
+          <Header />
+        </Flex>
+       
       </Screen>
     </ThemeProvider>
   )
